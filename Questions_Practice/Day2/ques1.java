287e60f8749a1bfe9fc2613455bf73687cdd42a4
Manish Saini <manishsainigvp12@gmail.com>
2025-12-30 22:28:26 +0530

    Day 2  practice Done

---
+++ Questions_Practice/Day2/ques1.java
@@ -0,0 +1,118 @@
+package com.day_1;
+import java.util.Scanner;
+public class ques1 {
+	
+	public static void main(String[] args) {
+//		Area Of Circle Java Program1
+		Scanner sc = new Scanner(System.in);
+//		System.out.print("Enter the radius of circle: ");
+//		float radius = sc.nextFloat();
+//		double pi = Math.PI;
+//		double area = pi*Math.pow(radius, 2);
+//		System.out.printf("Area of circle is : %.2f", area);
+		
+
+//		Area Of Triangle
+		
+//		float base,height,tarea;
+//		System.out.print("Enter the base and height of the triangle respectively: ");
+//		base = sc.nextFloat();
+//		height = sc.nextFloat();
+//		
+//		tarea = (base*height)/2;
+//		System.out.println("Area of triangle: "+tarea);
+		
+		
+		
+//		Area Of Rectangle Program
+		
+//		int length,breadth,area;
+//		System.out.println("Enter the length and breadth of rectangle: ");
+//		length = sc.nextInt();
+//		breadth = sc.nextInt();
+//		area = length*breadth;
+//		System.out.println("Area of rectangle :"+area);
+		
+//		Area Of Isosceles Triangle
+		
+//		float side,base,area;
+//		System.out.print("Enter the equal side and base of isoscales triangle: ");
+//		side = sc.nextFloat();
+//		base = sc.nextFloat();
+//		area = (float) ((base/4)*Math.sqrt(4*(side*side)-base*base));
+//		System.out.println("Area of Isosceles triangle: "+area);
+		
+//		Area Of Parallelogram
+//		float base,height,area;
+//		base = sc.nextFloat();
+//		height =sc.nextFloat();
+//		area = base*height;
+//		System.out.println("Area of Parallelogram: "+area);
+
+//		Subtract the Product and Sum of Digits of an Integer.
+		
+		
+//		Input a number and print all the factors of that number (use loops).
+		
+//		int num;
+//		System.out.print("Enter the Number to show factor: ");
+//		
+//		num = sc.nextInt();
+//		System.out.print("The factorial of Number "+num+" are: ");
+//		for(int i=1;i<=num;i++) {
+//			if(num %i == 0) {
+//				System.out.print(i+" ");
+//			}
+//		}
+		
+//		Take integer inputs till the user enters 0 and print the sum of all numbers (HINT: while loop)
+//		System.out.print("Enter the number except zero:  ");
+//		int num = sc.nextInt();
+//		if(num !=0) {
+//			
+//		int sum =0;
+//		while(num!=0) {
+//			sum = sum+num;
+//			System.out.print("Enter the number till the zero: ");
+//			num =sc.nextInt();
+//			
+//		}
+//		System.out.println("Here You Enter zero So the sum of All Number is: "+sum);
+//		
+//		}
+//		else {
+//			System.out.println("You already Enter the Zero(0)");
+//		}
+		
+		
+//		Take integer inputs till the user enters 0 and print the largest number from all.
+		
+//		System.out.print("Enter the number except zero:  ");
+//		int num = sc.nextInt();
+//		if(num !=0) {
+//			
+//		int max = 0;
+//		while(num!=0) {
+//			
+//			System.out.print("Enter the number till the zero: ");
+//			num =sc.nextInt();
+//			if(num > max) {
+//				max = num;
+//			}
+//			
+//		}
+//		System.out.println("Here You Enter zero So the sum of All Number is: "+max);
+//		
+//		}
+//		else {
+//			System.out.println("You already Enter the Zero(0)");
+//	         }
+		
+
+		
+		
+		
+
+		
+		}
+}
